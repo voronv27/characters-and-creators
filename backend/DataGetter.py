152982edfb70b4ae3f08ec02a0bcc5905991de10
voronv27 <152320775+voronv27@users.noreply.github.com>
2025-09-23 17:46:03 -0400

    Add more flask routes
    Add a route for information about DnD races, backgrounds, and preferred stats for a given class

--- backend/DataGetter.py
+++ backend/DataGetter.py
@@ -1,6 +1,25 @@
 import json
 import requests
 
+# Local data
+# List priority stats per DnD class
+preferredStats = {
+    "default": [],
+    "artificer": ["int", "con"],
+    "barbarian": ["str", "con"],
+    "bard": ["cha", "dex"],
+    "cleric": ["wis", "con"],
+    "druid": ["wis", "con"],
+    "fighter": ["str", "con"],
+    "monk": ["dex", "wis"],
+    "paladin": ["str", "cha"],
+    "ranger": ["dex", "wis"],
+    "rogue": ["dex", "int"],
+    "sorcerer": ["cha", "con"],
+    "warlock": ["cha", "con"],
+    "wizard": ["int", "con"],
+}
+
 # Helper functions to get data from the open5e API
 def getApiData(url, payload={}):
     response = requests.get(url, payload)
@@ -29,6 +48,8 @@ class DataGetter:
     """Gets and stores data from the open5e api"""
     def __init__(self):
         self.classData = None
+        self.races = None
+        self.backgrounds = None
 
     def getClasses(self):
         if not self.classData:
@@ -36,6 +57,24 @@ class DataGetter:
             self.classData = getData(url)
         return self.classData
 
+    def getRaces(self):
+        if not self.races:
+            url = "https://api.open5e.com/v1/races/"
+            races = getData(url)
+            self.races = {race['name']: race for race in races}
+        return self.races
+
+    def getBackgrounds(self):
+        if not self.backgrounds:
+            url = "https://api.open5e.com/v2/backgrounds/"
+            backgrounds = getData(url)
+            self.backgrounds = {bg['name']: bg for bg in backgrounds}
+        return self.backgrounds
+
+    def preferredStats(self, classname):
+        if classname in preferredStats:
+            return preferredStats[classname]
+        return preferredStats["default"]
 
 # Test DataGetter class
 if __name__ == "__main__":
@@ -45,3 +84,11 @@ if __name__ == "__main__":
         print(f"Class: {dndClass['name']}")
         subclasses = ', '.join([subclass['name'] for subclass in dndClass['archetypes']])
         print(f"{dndClass['subtypes_name']}: {subclasses}\n")
+
+    races = dataGetter.getRaces()
+    print("Races:", races.keys())
+
+    backgrounds = dataGetter.getBackgrounds()
+    print("Backgrounds:", backgrounds.keys())
+
+    print("Bard preferred stats are:", dataGetter.preferredStats["bard"])
--- backend/routes.py
+++ backend/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from DataGetter import DataGetter
 
 app = Flask(__name__)
@@ -14,5 +14,22 @@ def landing():
 def getClassNames():
     classData = dataGetter.getClasses()
     return [dndClass['name'] for dndClass in classData]
+
+# Returns { race: race info }
+@app.route('/races')
+def getRaces():
+    return dataGetter.getRaces()
+
+# Returns { background: background info }
+@app.route('/backgrounds')
+def getBackgrounds():
+    return dataGetter.getBackgrounds()
+
+# Given class name as a parameter, return the stats that should be highest
+@app.route('/preferred-stats')
+def preferredStats():
+    classname = request.args.get('class', default = None)
+    return dataGetter.preferredStats(classname)
+
 
 # TODO: add more routes for other DnD info
