47650af872214ed690b1187e0080da4033018057
voronv27 <152320775+voronv27@users.noreply.github.com>
2025-09-26 17:35:01 -0400

    Add class proficiencies to DataGetter, refactor classData
    Refactor classData to be a dictionary keyed by class names for easier access. Add in parsing for class proficiencies. The skills in the API are listed in a string, so I created a function to parse the string properly.

--- backend/DataGetter.py
+++ backend/DataGetter.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import random
 
 # Local data
 # List priority stats per DnD class
@@ -18,6 +19,42 @@ preferredStats = {
     "sorcerer": ["cha", "con"],
     "warlock": ["cha", "con"],
     "wizard": ["int", "con"],
+}
+
+# Mapping of word to number for API parsing purposes
+number = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "ten": 10,
+}
+
+# DnD skills and the corresponding stat they use (not in API)
+skills = {
+    "Athletics": "str",
+    "Acrobatics": "dex",
+    "Sleight of Hand": "dex",
+    "Stealth": "dex",
+    "Arcana": "int",
+    "History": "int",
+    "Investigation": "int",
+    "Nature": "int",
+    "Religion": "int",
+    "Animal Handling": "wis",
+    "Insight": "wis",
+    "Medicine": "wis",
+    "Perception": "wis",
+    "Survival": "wis",
+    "Deception": "cha",
+    "Intimidation": "cha",
+    "Performance": "cha",
+    "Persuasion": "cha",
 }
 
 # Helper functions to get data from the open5e API
@@ -42,6 +79,28 @@ def getData(url, payload={}):
             break
     return totalData
 
+# The open5e API lists skills in the format "Choose x skills from a, b, c, and d" or "Choose any x"
+# We want to parse this into a dictionary of { "count": x, "options": [a, b, c, d] }
+def getSkills(skillDesc):
+    count = 0
+    options = []
+
+    # Remove filler words
+    fillerWords = ["Choose ", "from ", "skills ", "and "]
+    filteredDesc = skillDesc
+    for fillerWord in fillerWords:
+        filteredDesc = filteredDesc.replace(fillerWord, "")
+    splitDesc = filteredDesc.split()
+    if splitDesc[0] == "any":
+        countWord = splitDesc[1]
+        options = list(skills.keys())
+    else:
+        countWord = splitDesc[0]
+        filteredDesc = filteredDesc.replace(f"{countWord} ", "")
+        options = filteredDesc.split(", ")
+
+    return {"count": number[countWord], "options": options}
+
 # DataGetter is imported by routes.py to get DnD data.
 # It caches this data to avoid making extra API calls.
 class DataGetter:
@@ -54,7 +113,8 @@ class DataGetter:
     def getClasses(self):
         if not self.classData:
             url = "https://api.open5e.com/v1/classes/"
-            self.classData = getData(url)
+            classData = getData(url)
+            self.classData = {classname["name"]: classname for classname in classData}
         return self.classData
 
     def getRaces(self):
@@ -76,19 +136,68 @@ class DataGetter:
             return preferredStats[classname]
         return preferredStats["default"]
 
+    def getClassProficiencies(self, classname):
+        proficiencies = {
+            "weapons": None,
+            "armors": None,
+            "tools": None,
+            "skills": None,
+            "savingThrows": None,
+        }
+        if not self.classData:
+            self.getClasses()
+
+        classData = self.classData[classname]
+        proficiencies["armors"] = classData["prof_armor"].split(", ")
+        proficiencies["weapons"] = classData["prof_weapons"].split(", ")
+        proficiencies["tools"] = classData["prof_tools"].split(", ")
+        proficiencies["savingThrows"] = classData["prof_saving_throws"].split(", ")
+        proficiencies["skills"] = getSkills(classData["prof_skills"])
+        return proficiencies
+
+    def getProficiencies(self, classname, race, background):
+        proficiencies = {
+            "class": None,
+            "race": None,
+            "background": None,
+        }
+        if not self.races:
+            self.getRaces()
+        if not self.backgrounds:
+            self.getBackgrounds()
+
+        # get class proficiencies
+        proficiencies["class"] = self.getClassProficiencies(classname)
+
+        # get race proficiencies
+        # TODO
+
+        # get background proficiencies
+        # TODO
+
+        return proficiencies
+
 # Test DataGetter class
 if __name__ == "__main__":
     dataGetter = DataGetter()
     classData = dataGetter.getClasses()
-    for dndClass in classData:
-        print(f"Class: {dndClass['name']}")
+    for dndClassName in classData:
+        dndClass = classData[dndClassName]
+        print(f"Class: {dndClassName}")
         subclasses = ', '.join([subclass['name'] for subclass in dndClass['archetypes']])
         print(f"{dndClass['subtypes_name']}: {subclasses}\n")
 
     races = dataGetter.getRaces()
-    print("Races:", races.keys())
+    print("Races:", list(races.keys()))
+    print()
 
     backgrounds = dataGetter.getBackgrounds()
-    print("Backgrounds:", backgrounds.keys())
+    print("Backgrounds:", list(backgrounds.keys()))
+    print()
 
-    print("Bard preferred stats are:", dataGetter.preferredStats["bard"])
+    sampleClass = random.choice(list(classData.keys()))
+    print(f"{sampleClass} preferred stats are: {list(dataGetter.preferredStats(sampleClass.lower()))}")
+    print()
+    print(f"{sampleClass} class proficiencies are {dataGetter.getClassProficiencies(sampleClass)}")
+
+
