a200b536f280d793382dff29727a9759cf13a97c
Garrett Gee <garrett.m.gee@gmail.com>
2025-12-09 20:40:11 -0500

    fix colors

--- main.js
+++ main.js
@@ -273,12 +273,15 @@ let char = {
     $("#export-pdf").click(function () {
       //Export format
       $("#character-sheet-parent").addClass("export-format");
-      html2pdf($("#character-sheet")[0], {
+      html2pdf($("#character-sheet-parent")[0], {
         filename: 'character-sheet.pdf',
         html2canvas: { scale: 1 },
         jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
       });
-      $("#character-sheet").removeClass("export-format");
+      setTimeout(function () {
+        $("#character-sheet-parent").removeClass("export-format");
+      }, 1000);
+
     });
   }
 
