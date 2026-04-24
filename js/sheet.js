84f056a8815d8d6a3720d55d24a14f2624637026
voronv27 <152320775+voronv27@users.noreply.github.com>
2026-04-24 05:14:25 -0400

    Fill in character sheet
    Will fill in values based on what we currently store in char. Some of the form field names are tricky and have hidden spaces. If you need to see them, just uncomment the code to do so, I've made sure to wrap names in quotes so extra space is visible.

--- js/sheet.js
+++ js/sheet.js
@@ -1,9 +1,10 @@
 // sheet.js has js for the character sheet
 
 // loads the pdf
-// when we pass in fieldnames and values we can update the
-// fillable entries on the pdf
-async function loadPdf(name, id, parentId, fields=null) {
+// when fields is non-null we can update the fillable
+// entries on the pdf
+var fields = {};
+async function loadPdf(name, id, parentId) {
 	const dir = `assets/character_sheet/${name}.pdf`;
 	const pdfBytes = await fetch(dir).then(res => res.arrayBuffer());
 	
@@ -13,7 +14,7 @@ async function loadPdf(name, id, parentId, fields=null) {
 	// used this to find names of form fields
 	/*const fieldNames = form.getFields();
 	fieldNames.forEach(f => {
-		console.log("name", f.getName());
+		console.log(`name: '${f.getName()}``);
 		console.log("type", f.constructor.name);
 	})*/
 
@@ -25,18 +26,16 @@ async function loadPdf(name, id, parentId, fields=null) {
 	const parentCont = document.getElementById(parentId);
 	parentCont.style.aspectRatio = `${width} / ${height}`;
 
-	if (fields) {
-		for (const fieldName in fields) {
-			const type = fields[fieldName]["type"];
-			const value = fields[fieldName]["value"];
-			if (type == "text") {
-				form.getTextField(fieldName).setText(value);
-			} else if (type == "checkbox") {
-				form.getCheckBox(fieldName).check();
-			}
+	for (const fieldName in fields) {
+		const type = fields[fieldName]["type"];
+		const value = fields[fieldName]["value"];
+		if (type == "text") {
+			form.getTextField(fieldName).setText(value);
+		} else if (type == "checkbox") {
+			form.getCheckBox(fieldName).check();
 		}
-		const updatedBytes = await pdfDoc.save();
 	}
+	const updatedBytes = await pdfDoc.save();
 	const pdfDataUri = await pdfDoc.saveAsBase64({dataUri: true});
 	
 	// removes any toolbars or margins or scroll, make zoom to fit
@@ -45,6 +44,171 @@ async function loadPdf(name, id, parentId, fields=null) {
 	pdfElement.src = pdfDataUri + adjustments;
 }
 
-loadPdf("CharacterSheet-1", "page1", "full-page1");
-loadPdf("CharacterSheet-2", "page2", "full-page2");
-loadPdf("basic_details", "basic-info", "basic-info-block");
+async function loadPdfPages() {
+	// pages
+	loadPdf("CharacterSheet-1", "page1", "full-page1");
+	loadPdf("CharacterSheet-2", "page2", "full-page2");
+	loadPdf("CharacterSheet-3", "page3", "full-page3");
+
+	// individual components
+	// todo
+	loadPdf("basic_details", "basic-info", "basic-info-block");
+}
+loadPdfPages();
+
+// goes through char and populates a fields dictionary,
+// then calls loadPdf to update character sheet
+const charToForm = {
+	"name": "CharacterName",
+	"class": "ClassLevel",
+	"race": "Race ",
+	"subrace": "Race ",
+	"background": "Background",
+	"stats": {
+		"Strength": "STR", 
+		"Dexterity": "DEX", 
+		"Constitution": "CON",
+		"Intelligence": "INT", 
+		"Wisdom": "WIS",
+		"Charisma": "CHA"
+	},
+	"proficiencies": {
+		"savingThrows": {
+			"Strength": "Check Box 11", 
+			"Dexterity": "Check Box 18", 
+			"Constitution": "Check Box 19",
+			"Intelligence": "Check Box 20", 
+			"Wisdom": "Check Box 21",
+			"Charisma": "Check Box 22"
+		},
+		"skills": {
+		    "Acrobatics": "Check Box 23",
+		    "Animal Handling": "Check Box 24",
+		    "Arcana": "Check Box 25",
+		    "Athletics": "Check Box 26",
+		    "Deception": "Check Box 27",
+		    "History": "Check Box 28",
+		    "Insight": "Check Box 29",
+		    "Intimidation": "Check Box 30",
+		    "Investigation": "Check Box 31",
+		    "Medicine": "Check Box 32",
+		    "Nature": "Check Box 33",
+		    "Perception": "Check Box 34",
+		    "Performance": "Check Box 35",
+		    "Persuasion": "Check Box 36",
+		    "Religion": "Check Box 37",
+		    "Sleight of Hand": "Check Box 38",
+		    "Stealth": "Check Box 39",
+		    "Survival": "Check Box 40"
+		},
+		"armors": "ProficienciesLang",
+		"weapons": "ProficienciesLang",
+		"tools": "ProficienciesLang"
+	}
+}
+const textFields = new Set([
+	"CharacterName", "ClassLevel", "Race ", "Background",
+	"STR", "DEX", "CON", "INT", "WIS", "CHA", 
+	"ProficienciesLang"
+])
+async function updatePdf() {
+	fields = {};
+	for (entry in charToForm) {
+		// skip null and empty values
+		if (!char[entry]) {
+			continue;
+		}
+		if (typeof char[entry] == "object" && 
+			Object.keys(char[entry]).length == 0) {
+			continue;
+		}
+
+		const charEntry = char[entry];
+		var formField;
+		if (entry in charToForm) {
+			formField = charToForm[entry];
+		}
+
+		var value = "";
+		if (formField && formField in fields) {
+			value = fields[formField].value;
+		}
+
+		// handle special cases (there's a lot)
+		if (entry == "class") {
+			for (c in charEntry) {
+				if (value != "") {
+					value += ", ";
+				}
+				value += c;
+				if (charEntry[c].subclass) {
+					value += ` (${charEntry[c].subclass})`
+				}
+				value += ` Lv ${charEntry[c].level}`;
+			}
+		} else if (entry == "subrace") {
+			value += ` (${charEntry})`;
+		} else if (entry == "stats") {
+			for (s in charEntry) {
+				value = charEntry[s];
+				if (value) {
+					const formInfo = {};
+					formInfo.type = "text";
+					formInfo.value = `${value}`;
+					formField = charToForm["stats"][s];
+					fields[formField] = formInfo;
+				}
+			}
+			continue;
+		} else if (entry == "proficiencies") {
+			for (profType in charToForm["proficiencies"]) {
+				if (profType == "savingThrows") {
+					for (st of charEntry.savingThrows) {
+						const formInfo = {};
+						formInfo.type = "checkbox";
+						formField = charToForm["proficiencies"]["savingThrows"][st];
+						fields[formField] = formInfo;
+					}
+				} else if (profType == "skills") {
+					for (sk of charEntry.skills) {
+						const formInfo = {};
+						formInfo.type = "checkbox";
+						formField = charToForm["proficiencies"]["skills"][sk];
+						fields[formField] = formInfo;
+
+						console.log(sk);
+						console.log(formField);
+					}
+				}
+				else {
+					value += charEntry[profType].join(", ") + "\n";
+					formField = charToForm["proficiencies"][profType];
+					const formInfo = {};
+					formInfo.type = "text";
+					formInfo.value = value;
+					fields[formField] = formInfo;
+				}
+			}
+			continue;
+		} else {
+			value += charEntry;
+		}
+
+		const formInfo = {};
+		formInfo.type = "checkbox";
+		if (textFields.has(formField)) {
+			formInfo.type = "text";
+			formInfo.value = value;
+		}
+		fields[formField] = formInfo;
+	}
+	await loadPdf("CharacterSheet-1", "page1", "full-page1");
+}
+
+// listens for when we reach the character sheet page and
+// populates sheet with values from char
+window.addEventListener('hashchange', () => {
+    if (sectionNum == numSections - 1) {
+    	updatePdf();
+    }
+});
